1c01942eb07f426fbd989614ca0f7c39551b07e0
farzanapomy <farzanapomy56@gmail.com>
2025-01-09 22:26:52 +0600

    another-test

--- random problem/random test/secondLastDigit.cpp
+++ random problem/random test/secondLastDigit.cpp
@@ -1,15 +1,22 @@
 // https://www.codechef.com/problems/PHONEYR
 #include <bits/stdc++.h>
 using namespace std;
+// int main()
+// {
+//     int x0;
+//     cin >> x0;
+//     int y0 = x0 / 10;
+//     int z0 = y0 * 10;
+//     z0 = x0 - z0;
+//     int y1 = y0 / 10;
+//     int z1 = y1 * 10;
+//     z1 = y0 - z1;
+//     cout << "K" << z1 << z0 << endl;
+// }
+
 int main()
 {
-    int x0;
-    cin >> x0;
-    int y0 = x0 / 10;
-    int z0 = y0 * 10;
-    z0 = x0 - z0;
-    int y1 = y0 / 10;
-    int z1 = y1 * 10;
-    z1 = y0 - z1;
-    cout << "K" << z1 << z0 << endl;
+    char d1, d2, d3, d4;
+    cin >> d1 >> d2 >> d3 >> d4;
+    cout << "K" << d3 << d4 << endl;
 }
