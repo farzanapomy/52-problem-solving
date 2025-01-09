41ca3563990293bf5492385a6285a61db00d824f
farzanapomy <farzanapomy56@gmail.com>
2025-01-09 22:21:47 +0600

    solve-codechef.541

--- random problem/random test/secondLastDigit.cpp
+++ random problem/random test/secondLastDigit.cpp
@@ -1,14 +1,15 @@
+// https://www.codechef.com/problems/PHONEYR
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int x0 = 2024;
+    int x0;
+    cin >> x0;
     int y0 = x0 / 10;
     int z0 = y0 * 10;
     z0 = x0 - z0;
-    cout << x0 << " " << y0 << " " << z0 << endl;
-    int y1 = y0 / 10; // 20
-    int z1 = y1 * 10; //200
+    int y1 = y0 / 10;
+    int z1 = y1 * 10;
     z1 = y0 - z1;
-    cout << x0 << " " << y0 << " " << z0 << " " << z1 << endl;
+    cout << "K" << z1 << z0 << endl;
 }
---
+++ random problem/random test/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+int x0;
+    cin >> x0;
+    int y0 = x0 / 10;
+    int z0 = y0 * 10;
+    z0 = x0 - z0;
+    int y1 = y0 / 10;
+    int z1 = y1 * 10;
+    z1 = y0 - z1;
+    cout << "K" << z1 << z0 << endl;
